afade07be190708125a7e4c27beb83ae247de894
balasrikanth48 <balasrikanth48@gmail.com>
2022-09-25 21:13:48 +0530

    second commit

---
+++ src/test/Test2.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Test2 
+{
+	
+	
+	
+	public Test2()
+	{
+		
+		System.out.println("hi");
+		
+	}
+	
+}
--- src/test/test1.java
+++ src/test/test1.java
@@ -8,11 +8,6 @@ public class test1 {
 
 	public static void main(String[] args) 
 	{
-		System.setProperty("webdriver.chrome.driver" ,"D:\\\\Chrome\\\\chromedriver.exe");
-		WebDriver driver=new ChromeDriver();
-		driver.get("https:\\www.google.com");
-		//driver.findElement(By.className("M6CB1c rr4y5c")).click();
-		driver.findElement(By.xpath("//div[@class='FPdoLc lJ9FBc']//input[@name='btnI']")).click();
 		
 		
 		
